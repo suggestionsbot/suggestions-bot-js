b5ee46e1d25b03ba26c01b721f2c3e22916dc7cd
Anthony Collier <gamemaniac101@gmail.com>
2018-09-29 00:51:30 -0400

    sid and suggestions commands can be used by anyone now

--- commands/sid.js
+++ commands/sid.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const moment = require('moment');
 const Settings = require('../models/settings.js');
 const Suggestion = require('../models/suggestions.js');
-const { noPerms, maintenanceMode, noSuggestionsPerms } = require('../utils/errors.js');
+const { maintenanceMode } = require('../utils/errors.js');
 const { orange, owner } = require('../config.json');
 require('moment-duration-format');
 require('moment-timezone');
@@ -23,21 +23,6 @@ exports.run = async (client, message, args) => {
         guildID: message.guild.id,
     }, async (err, res) => {
         if (err) return console.log(err);
-
-        const roles = res.staffRoles;
-
-        const staffRoles = roles.map(el => {
-            return message.guild.roles.find(r => r.name === el.role || r.id === el.role);
-        });
-
-        let admins = [];
-        message.guild.members.forEach(collected => {
-            
-            if (collected.hasPermission('MANAGE_GUILD') && !collected.user.bot) return admins.push(collected.id);
-            
-        });
-
-        if (!admins.includes(message.member.id) && !message.member.roles.some(r => staffRoles.includes(r))) return noSuggestionsPerms(message.channel);
 
         let id = args[0];
         if (!id) return message.channel.send(`Usage: \`${res.prefix + cmdName} <id>\``).then(msg => msg.delete(5000)).catch(console.error);
