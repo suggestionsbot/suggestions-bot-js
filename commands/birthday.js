e3d72862a03868a32f2ae862099dfaae71e7703f
acollierr17 <gamemaniac101@gmail.com>
2018-10-19 16:17:22 -0400

    Fix typo in embed

--- commands/birthday.js
+++ commands/birthday.js
@@ -4,7 +4,7 @@ const { RichEmbed } = require('discord.js');
 exports.run = async (client, message, args) => {
 
 	let embed = new RichEmbed()
-		.setDescription(`Its <@${owner}>'s birthday! (born on October 19, 1998 somewhere around 6AM.`)
+		.setDescription(`Its <@${owner}>'s birthday! (born on October 19, 1998 somewhere around 6AM.)`)
 		.setColor(embedColor);
 
 	message.channel.send(embed).then(msg => msg.delete(5000)).catch(console.error);
