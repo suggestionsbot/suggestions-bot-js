279f2468c98b091feac566fdc8b4a63421a03f96
acollierr17 <gamemaniac101@gmail.com>
2018-11-01 03:29:14 -0400

    Another test

--- events/ready.js
+++ events/ready.js
@@ -23,71 +23,71 @@ module.exports = async client => {
 
     // If the bot was invited to a guild while it was offline, the "ready" event will
     // be emitted (ONLY IN PRODUCTION)
-    // if (process.env.NODE_ENV === 'production') {
-    //     client.guilds.forEach(async g => {
-    //         let gSettings = await Settings.findOne({ guildID: g.id }).catch(console.error);
+    if (process.env.NODE_ENV === 'production') {
+        client.guilds.forEach(async g => {
+            let gSettings = await Settings.findOne({ guildID: g.id }).catch(console.error);
             
-    //         if (!gSettings && g) await client.emit('guildCreate', g);
+            if (!gSettings && g) await client.emit('guildCreate', g);
             
-    //     });
+        });
 
-    //     // Need to figure out a more efficient way of doing this I feel. Emitting the
-    //     // "guildDelete" event doesn't seem the best way to do things because the guild
-    //     // doesn't actually exist!
-    //     let gSettings = await Settings.find().catch(console.error);
-    //     gSettings.map(async e => {
-    //         let g = client.guilds.get(e.guildID);
-    //         if (!g) {
-    //             await Settings.findOneAndDelete({ guildID: e.guildID }, err => {
-    //                 if (err) console.log(err);
+        // Need to figure out a more efficient way of doing this I feel. Emitting the
+        // "guildDelete" event doesn't seem the best way to do things because the guild
+        // doesn't actually exist!
+        let gSettings = await Settings.find().catch(console.error);
+        gSettings.map(async e => {
+            let g = client.guilds.get(e.guildID);
+            if (!g) {
+                await Settings.findOneAndDelete({ guildID: e.guildID }, err => {
+                    if (err) console.log(err);
                     
-    //                 console.log(`Settings data deleted for guild ${e.guildName} (${e.guildID})`);
-    //             });
+                    console.log(`Settings data deleted for guild ${e.guildName} (${e.guildID})`);
+                });
             
-    //             await Suggestion.deleteMany({ guildID: e.guildID }, err => {
-    //                 if (err) console.log(err);
+                await Suggestion.deleteMany({ guildID: e.guildID }, err => {
+                    if (err) console.log(err);
             
-    //                 console.log(`Suggestions data deleted for guild ${e.guildName} (${e.guildID})`);
-    //             });
+                    console.log(`Suggestions data deleted for guild ${e.guildName} (${e.guildID})`);
+                });
             
-    //             await Command.deleteMany({ guildID: e.guildID }, err => {
-    //                 if (err) console.log(err);
+                await Command.deleteMany({ guildID: e.guildID }, err => {
+                    if (err) console.log(err);
             
-    //                 console.log(`Command data deleted for guild ${e.guildName} (${e.guildID})`);
+                    console.log(`Command data deleted for guild ${e.guildName} (${e.guildID})`);
             
-    //             });
+                });
 
-    //             console.log(`${client.user.username} has left a guild (bot was offline): ${e.guildName} (${e.guildID})`);
+                console.log(`${client.user.username} has left a guild (bot was offline): ${e.guildName} (${e.guildID})`);
 
-    //             botPresence(client);
+                botPresence(client);
 
-    //             let oldServer = new RichEmbed()
-    //                 .setTitle('Removed')
-    //                 .setDescription(`
-    //                     **ID:** \`${e.guildID}\`
-    //                     **Name:** \`${e.guildName}\`
-    //                     **Owner:** <@${e.guildOwnerID}>
-    //                 `)
-    //                 .setColor('#FF4500')
-    //                 .setTimestamp();
+                let oldServer = new RichEmbed()
+                    .setTitle('Removed')
+                    .setDescription(`
+                        **ID:** \`${e.guildID}\`
+                        **Name:** \`${e.guildName}\`
+                        **Owner:** <@${e.guildOwnerID}>
+                    `)
+                    .setColor('#FF4500')
+                    .setTimestamp();
 
-    //             switch (process.env.NODE_ENV) {
-    //                 // 345753533141876737 = Nerd Cave Testing
-    //                 case 'development': {
-    //                     const logGuild = client.guilds.get('345753533141876737');
-    //                     const logChannel = logGuild.channels.find(c => c.name === 'server_logs');
-    //                     logChannel.send(oldServer);
-    //                     break;
-    //                 }
-    //                 // 480231440932667393 = Nerd Cave Development
-    //                 default: {
-    //                     const logGuild = client.guilds.get('480231440932667393');
-    //                     const logChannel = logGuild.channels.find(c => c.name === 'server_logs');
-    //                     logChannel.send(oldServer);
-    //                     break;
-    //                 }
-    //             }
-    //         }
-    //     });
-    // }
+                switch (process.env.NODE_ENV) {
+                    // 345753533141876737 = Nerd Cave Testing
+                    case 'development': {
+                        const logGuild = client.guilds.get('345753533141876737');
+                        const logChannel = logGuild.channels.find(c => c.name === 'server_logs');
+                        logChannel.send(oldServer);
+                        break;
+                    }
+                    // 480231440932667393 = Nerd Cave Development
+                    default: {
+                        const logGuild = client.guilds.get('480231440932667393');
+                        const logChannel = logGuild.channels.find(c => c.name === 'server_logs');
+                        logChannel.send(oldServer);
+                        break;
+                    }
+                }
+            }
+        });
+    }
 };
