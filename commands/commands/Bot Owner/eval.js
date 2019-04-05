48c05b4c0e56a6b66e13be018d48dbbb6a10b3a2
acollierr17 <gamemaniac101@gmail.com>
2019-04-04 23:56:57 -0400

    Update eval command

--- commands/commands/Bot Owner/eval.js
+++ commands/commands/Bot Owner/eval.js
@@ -1,3 +1,4 @@
+const { RichEmbed } = require('discord.js');
 const hastebin = require('hastebin-gen');
 const Command = require('../../Command');
 
@@ -8,33 +9,62 @@ module.exports = class EvalCommand extends Command {
             category: 'Bot Owner',
             description: 'Run raw Javascript code via the bot.',
             usage: 'eval <code>',
-            ownerOnly: true,
-            guildOnly: false
+            ownerOnly: true
         });
     }
 
-    async run(message, args, settings) {
+    async run(message, args) {
 
-        const code = args.join(' ');
-        if (!code) return this.client.errors.noUsage(message.channel, this, settings);
+        const { embedColor, suggestionColors: { rejected } } = this.client.config;
+
+        let code = args.join(' ');
+        if (!code) return this.client.errors.noUsage(message.channel, this);
+
+        const embed = new RichEmbed();
+        const exceededEmbed = new RichEmbed();
 
         try {
             const evaled = eval(code);
             const clean = await this.client.clean(evaled);
+
             // 6 graves, and 2 characters for "js"
             const MAX_CHARS = 3 + 2 + clean.length + 3;
             if (MAX_CHARS > 2000) {
-                const haste = await hastebin(Buffer.from(clean), 'js');
-                return message.channel.send('Output exceeded 2000 characters. DMing you the Hastebin.')
-                    .then(msg => {
-                        message.author.send(`<${haste}>`);
-                        msg.react('📧').then(() => msg.delete(5000));
-                    })
-                    .catch(err => this.client.logger.error(err.stack));
+                const haste = await hastebin(Buffer.from(clean), {
+                    url: 'https://paste.thenerdcave.us',
+                    extension: 'js'
+                });
+                message.author.send(`<${haste}>`);
+
+                exceededEmbed.setColor(embedColor);
+                exceededEmbed.setDescription('📨 Output exceeded 2000 characters. DMing you the Hastebin.');
+
+                const msg = await message.channel.send(exceededEmbed);
+                await msg.react('📧');
+                await msg.delete(2500);
+                return;
             }
-            return message.channel.send(clean, { code: 'js' });
         } catch (err) {
-            message.channel.send(await this.client.clean(err), { code: 'bash' });
+            if (err.length > 2000) {
+                const haste = await hastebin(Buffer.from(err), {
+                    url: 'https://paste.thenerdcave.us',
+                    extension: 'js'
+                });
+                message.author.send(`<${haste}>`);
+
+                exceededEmbed.setColor(rejected);
+                exceededEmbed.setDescription('📨 Output exceeded 2000 characters. DMing you the Hastebin.');
+
+                const msg = await message.channel.send(exceededEmbed);
+                await msg.react('📧');
+                await msg.delete(5000);
+                return;
+            }
+
+            embed.setColor(rejected);
+            embed.addField('Error ❗', `\`\`\`bash\n${err}\`\`\``);
         }
+
+        if (!code.startsWith('void')) return message.channel.send(embed);
     }
 };
