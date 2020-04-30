2909d6cc0a9cffe77e2635e9826c2969b2b1e956
acollierr17 <gamemaniac101@gmail.com>
2020-04-30 13:52:55 -0700

    Update event name due to v12 change

--- sharding.js
+++ sharding.js
@@ -12,7 +12,7 @@ const manager = new ShardingManager('./bot.js', {
 });
 
 manager.spawn();
-manager.on('launch', shard => logger.log(`Launched shard ${shard.id}`, 'ready'));
+manager.on('shardCreate', shard => logger.log(`Launched shard ${shard.id}`, 'ready'));
 manager.on('message', (shard, message) => {
   logger.log(`Shard[${shard.id}] : ${message._eval} : ${message._result}`);
 });
