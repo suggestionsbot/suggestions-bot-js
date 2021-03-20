705c7eec0d33b92dabde096d3b72a5b6f3ffdab7
Anthony Collier <hello@acollier.dev>
2021-03-19 22:56:03 -0400

    :art: Add shard and cluster ids

--- commands/commands/General/ping.js
+++ commands/commands/General/ping.js
@@ -17,10 +17,11 @@ module.exports = class PingCommand extends Command {
   async run(message, args) {
     try {
       const msg = await message.channel.send('🏓 Ping!');
+      const [shard, cluster] = [message.guild ? message.guild.shardID : 0, this.client.shard.id];
       return msg.edit(oneLine`
         Pong!
-        Latency is \`${msg.createdTimestamp - message.createdTimestamp}ms\`.
-        API Latency is \`${Math.round(this.client.ws.ping)}ms\`.
+        **Latency:** \`${msg.createdTimestamp - message.createdTimestamp}ms\`.
+        **Shard ${shard} - Cluster ${cluster}:** \`${Math.round(this.client.ws.ping)}ms\`.
       `);
     } catch (e) {
       this.client.logger.error(e);
