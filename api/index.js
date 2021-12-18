846ec19327bcfc8ee6dba47131814b613867fae7
Anthony Collier <hello@acollier.dev>
2021-12-17 23:59:29 -0500

    :fire: Remove dashboard/API code

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const Server = require('./server/Server');
-
-module.exports = class DashboardClient {
-  constructor(client) {
-    this.app = new Server(client);
-
-    // this.app.listen(4000);
-  }
-};
